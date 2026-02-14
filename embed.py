99536579a8e5c0f18280c2898b045391a0e4d332
Arindam Bhattacharjee <inferno.arindam@outlook.com>
2026-02-14 18:57:36 +0000

    feat: created container image

--- embed.py
+++ embed.py
@@ -2,7 +2,7 @@ import chromadb
 
 client = chromadb.PersistentClient(path="./chroma_db")
 collection = client.get_or_create_collection("docs")
-
+    
 with open("k8s.txt", "r") as f:
     text = f.read()
 
